317f7897e8d638c7456394f3b87634e2a4061cc7
Emanuel Andrade <emanuelpna.900034017@uniacademia.edu.br>
2024-10-31 15:49:45 -0300

    corrige test_modifyuser_nome_usuarios

--- website/tests/modifyuser/test_modifyuser.py
+++ website/tests/modifyuser/test_modifyuser.py
@@ -1,11 +1,24 @@
-from utils import send_GET_request, send_POST_request
+from utils import send_GET_request, send_POST_request, encontra_item_na_lista
 
-# def test_modifyuser_nome_usuarios(client, captured_templates):
-#     data = send_POST_request('/modifyuser', {'first_name': 'maria'}, client, captured_templates)
-#     output_data = data.get('output_data')
-#     assert type(output_data) == list
-#     lista_nome = list(filter(lambda x: x[1] == 'maria', output_data))
-#     assert len(lista_nome) > 0
+def test_modifyuser_nome_usuarios(client, captured_templates):
+    maria_user = {
+        'email': 'maria@email.com',
+        'firstName': 'maria', 
+        'password1': '1234567', 
+        'password2': '1234567'
+    }
+
+    send_POST_request('/sign-up', maria_user, client, captured_templates)
+        
+    data = send_GET_request('/modifyuser', client, captured_templates)
+    
+    output_data = data.get('output_data')
+    
+    assert type(output_data) == list
+
+    lista_nome = encontra_item_na_lista(output_data, 1, 'maria')
+
+    assert len(lista_nome) > 0
 
 def test_modifyuser_listagem_usuarios(client, captured_templates):
     data = send_GET_request('/modifyuser', client, captured_templates)
