dc447cb90aea000d32a5cc7703dd8486b797f76f
Emanuel Andrade <emanuelpna.900034017@uniacademia.edu.br>
2024-10-31 15:49:42 -0300

    cria nova função util para encontrar item numa lista

--- website/tests/utils.py
+++ website/tests/utils.py
@@ -1,5 +1,3 @@
-import inspect
-
 def send_GET_request(url, client, captured_templates, params=None):
     client.get(url, query_string=params)
 
@@ -17,3 +15,20 @@ def send_POST_request(url, body, client, captured_templates, params=None):
     template, data = captured_templates[-1]
 
     return data
+
+def encontra_item_na_lista(lista, index_da_tupla, campo):
+    """
+        Procura um item dentro de uma lista de tuplas
+
+        As duas funções acima de requisição HTTP retorna uma lista com os dados em formato de tupla, como:
+        (1, 'Nome da Linha de Pesquisa', 1, 'Nome do Usuário') => (research_line_id, research_line_name, user_id_log, user_name_log)
+
+        Para saber a ordem de uma tupla não é muito intuitivo. O jeito mais rápido é fazer um `assert` que force um erro como `assert list == 1`
+        Isso por que o pytest irá printar as tuplas que estiverem na lista e será possível ver a ordem e escolher o index necessário
+    """
+
+    result = list(filter(lambda x: x[index_da_tupla] == campo, lista))
+
+    assert len(result) == 1
+
+    return result[0]
