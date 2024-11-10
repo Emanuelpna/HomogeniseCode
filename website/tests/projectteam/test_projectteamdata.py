a4b164be559a5a99c7919e0d80121a6c52169ea9
SenpSenp <lucas3.14ccinini@gmail.com>
2024-11-10 19:03:41 -0300

    Projectteamdata completo
    O caso TC014-A está com um problema por causa de que a send_post_request possui já um assert que garante nao receber body vazio, decidi não mexer para não atrapalhar o código já funcionando, imagino que a lógica do teste esteja certa mas não consegui testar por conta disso

--- website/tests/projectteam/test_projectteamdata.py
+++ website/tests/projectteam/test_projectteamdata.py
@@ -1,6 +1,125 @@
 from utils import send_POST_request
 from uuid import uuid4
 
+def test_projectteamdata_inserir_dados_ausentes(client, captured_templates):
+    """
+    [TC014-A] Verifica se o sistema impede a execução quando faltam dados obrigatórios.
+    """
+
+    body = {
+        'project_id': '',    
+        'user_id': '',       
+        'st_user_leader': ''   
+    }
+
+    params = {'type_operation': 'A'}
+
+    post_response = send_POST_request('/projectteamdata', body, client, captured_templates, params) # <- O erro acontece aqui porque no próprio send_post_request tem um assert que garante que os campos não estão vazios
+
+    if captured_templates:
+        messages = [message for message in captured_templates[0][1].get('_flashes', [])]
+        assert ('error', 'Fill out all data to execute transaction!') in messages
+    else:
+        assert False, "Nenhum template foi renderizado após a requisição POST."
+
+    output_data = post_response.get('output_data')
+    assert len(output_data) == 0
+
+def test_projectteamdata_deletar_membro(client, captured_templates):
+    """
+        [TC014-B] Deletar um membro de um projeto
+    """
+    
+    project_id = "1"
+    user_id = "2"
+    st_user_leader = "0"
+
+    post_response = send_POST_request('/projectteamdata', {
+        'project_id': project_id,
+        'user_id': user_id,
+        'st_user_leader': st_user_leader
+    }, client, captured_templates, { 'type_operation': 'A' })
+
+    output_data = post_response.get('output_data')
+    assert len(output_data) > 0
+    new_entry_id = output_data[-1][0]
+
+    body = {
+        'project_team_id': new_entry_id,
+        'project_id': project_id,
+        'user_id': user_id,
+        'st_user_leader': st_user_leader
+    }
+
+    params = { 'type_operation': 'D' }
+
+    post_response = send_POST_request('/projectteamdata', body, client, captured_templates, params)
+    
+    output_data = post_response.get('output_data')
+    deleted_entry = list(filter(lambda x: x[0] == new_entry_id, output_data))
+
+    assert len(deleted_entry) == 0
+
+def test_projectteamdata_inserir_membro(client, captured_templates):
+    """
+        [TC014-C] Inserir um membro em um time de projeto
+    """
+
+    project_id = "1"
+    user_id = "2"
+    st_user_leader = "0"
+
+    post_response = send_POST_request('/projectteamdata', {
+        'project_id': project_id,
+        'user_id': user_id,
+        'st_user_leader': st_user_leader
+    }, client, captured_templates, { 'type_operation': 'A' })
+
+    output_data = post_response.get('output_data')
+    assert len(output_data) > 0
+    new_entry_id = output_data[-1][0]
+
+    new_entry = list(filter(lambda x: x[0] == new_entry_id, output_data))
+
+    assert len(new_entry) == 1
+    assert new_entry[0][0] == new_entry_id
+    assert new_entry[0][1] == 'Projeto teste'
+    assert new_entry[0][2] == 'NewUser'
+    assert new_entry[0][3] == ''
+
+def test_projectteamdata_impedir_duplicidade_membros(client, captured_templates):
+    """
+        [TC014-D] Impedir duplicidade de membros em um projeto
+    """
+    
+    project_id = "1"
+    user_id = "2"
+    st_user_leader = "0"
+
+    post_response = send_POST_request('/projectteamdata', {
+        'project_id': project_id,
+        'user_id': user_id,
+        'st_user_leader': st_user_leader
+    }, client, captured_templates, { 'type_operation': 'A' })
+
+    output_data = post_response.get('output_data')
+    assert len(output_data) > 0
+    new_entry_id = output_data[-1][0]
+
+    body = {
+        'project_team_id': new_entry_id,
+        'project_id': project_id,
+        'user_id': user_id,
+        'st_user_leader': st_user_leader
+    }
+
+    params = { 'type_operation': 'A' }
+
+    post_response = send_POST_request('/projectteamdata', body, client, captured_templates, params)
+    
+    get_response = client.get(f'/projectteamdata/{project_id}')
+    team_members = get_response.get_json()
+
 def test_projectteamdata_atualizar_membro(client, captured_templates):
     """
         [TC014-E] Atualizar um membro em um projeto
