5375decc3c77959bb679c0e585e6f46c66eb49e1
Emanuel Andrade <emanuelpna.900034017@uniacademia.edu.br>
2024-11-24 12:39:30 -0300

    adicionando testes de nome de usuário

--- website/functional-tests/sign_up/test_sign_up.py
+++ website/functional-tests/sign_up/test_sign_up.py
@@ -45,7 +45,7 @@ def test_create_user_with_email_valid_length(firefox_browser):
     firefox_browser.get("http://web:5000/sign-up")
 
     input_email = firefox_browser.find_element(By.XPATH, '//*[@id="email"]')
-    input_email.send_keys("test")
+    input_email.send_keys("t@em") # A validação do input[type="email"] nos navegadores exige que tenha pelo menos um @ na string
 
     input_firstName = firefox_browser.find_element(By.XPATH, '//*[@id="firstName"]')
     input_firstName.send_keys("Nome do Usuário de Teste")
@@ -69,3 +69,58 @@ def test_create_user_with_email_valid_length(firefox_browser):
     except:
         # Deveria ter a mensagem de sucesso. Se não encontrou o elemento na tela força uma falha no teste
         assert False
+
+
+def test_create_user_with_username_invalid_length(firefox_browser):
+    firefox_browser.get("http://web:5000/sign-up")
+
+    input_email = firefox_browser.find_element(By.XPATH, '//*[@id="email"]')
+    input_email.send_keys("Test@gmail.com")
+
+    input_firstName = firefox_browser.find_element(By.XPATH, '//*[@id="firstName"]')
+    input_firstName.send_keys("T")
+
+    input_password1 = firefox_browser.find_element(By.XPATH, '//*[@id="password1"]')
+    input_password1.send_keys("1234567")
+
+    input_password2 = firefox_browser.find_element(By.XPATH, '//*[@id="password2"]')
+    input_password2.send_keys("1234567")
+
+    form_element = firefox_browser.find_element(By.XPATH, '/html/body/div/form')
+    form_element.submit()
+
+    time.sleep(1)
+
+    try:
+        flash_message_accountCreated = firefox_browser.find_element(By.CSS_SELECTOR, '.alert.alert-success[role="alert"]')
+    
+        assert not flash_message_accountCreated.is_displayed()
+    except:
+        assert True
+
+def test_create_user_with_username_valid_length(firefox_browser):
+    firefox_browser.get("http://web:5000/sign-up")
+
+    input_email = firefox_browser.find_element(By.XPATH, '//*[@id="email"]')
+    input_email.send_keys("Test@gmail.com")
+
+    input_firstName = firefox_browser.find_element(By.XPATH, '//*[@id="firstName"]')
+    input_firstName.send_keys("Te")
+
+    input_password1 = firefox_browser.find_element(By.XPATH, '//*[@id="password1"]')
+    input_password1.send_keys("1234567")
+
+    input_password2 = firefox_browser.find_element(By.XPATH, '//*[@id="password2"]')
+    input_password2.send_keys("1234567")
+
+    form_element = firefox_browser.find_element(By.XPATH, '/html/body/div/form')
+    form_element.submit()
+
+    time.sleep(1)
+
+    try:
+        flash_message_accountCreated = firefox_browser.find_element(By.CSS_SELECTOR, '.alert.alert-success[role="alert"]')
+    
+        assert not flash_message_accountCreated.is_displayed()
+    except:
+        assert True
